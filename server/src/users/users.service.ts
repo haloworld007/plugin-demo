efc7707d7f12f40ceb35a4f7d712a4d091bf0c41
trhuo <891567977@qq.com>
2025-04-07 11:49:11 +0800

    add extra server

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    { id: 1, name: '张三', email: 'zhangsan@example.com' },
+    { id: 2, name: '李四', email: 'lisi@example.com' },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+}
