fcf19c8711f9458a9f96490db4c9e0e98ec22d87
trhuo <891567977@qq.com>
2025-04-09 17:42:24 +0800

    update dependency

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // 导入其他模块
 import { UsersModule } from './users/users.module';
-import { PluginsModule } from './plugins/plugins.module';
+// import { PluginsModule } from './plugins/plugins.module';
+import { PluginsModule } from '@mora-credit/plugin-server';
 
 @Module({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   imports: [UsersModule, PluginsModule.register()],
   controllers: [AppController],
   providers: [AppService],
