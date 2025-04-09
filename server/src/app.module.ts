75e239b3e36770fb4503e31aa304c26dbfab3175
trhuo <891567977@qq.com>
2025-04-09 16:35:08 +0800

    feat: perfect code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,22 +1,13 @@
-import { Module, DynamicModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // 导入其他模块
 import { UsersModule } from './users/users.module';
 import { PluginsModule } from './plugins/plugins.module';
 
-@Module({})
-export class AppModule {
-  static async register(): Promise<DynamicModule> {
-    return {
-      module: AppModule,
-      imports: [
-        UsersModule,
-        // 导入插件模块，它会负责发现和加载所有插件
-        await PluginsModule.register(),
-      ],
-      controllers: [AppController],
-      providers: [AppService],
-    };
-  }
-}
+@Module({
+  imports: [UsersModule, PluginsModule.register()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // 使用register方法动态加载模块
-  const appModule = AppModule.register();
-  const app = await NestFactory.create(appModule);
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 
